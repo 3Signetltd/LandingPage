27d2103bfaa4ae2b19ffe2b023c6a246f20eef17
pmanonline <petersonzoconis@gmail.com>
2024-12-27 06:00:44 +0100

    updated registrayion form

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const backendURL = "http://localhost:3002";
-// const backendURL = "https://3signet-backend.vercel.app";
+// const backendURL = "http://localhost:3002";
+const backendURL = "https://3signet-backend.vercel.app";
 
 export default backendURL;
